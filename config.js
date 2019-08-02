84d74c63fcdd58a1ad8a416fd52290c3dc09c0f8
Michael Peterson <WebDev@ctoms.ca>
2019-08-02 13:10:39 -0600

    setup db connection

---
+++ config.js
@@ -0,0 +1,6 @@
+exports.PORT = process.env.PORT || 8000;
+exports.CKEY = process.env.CKEY;
+exports.URLCAD = process.env.URLCAD;
+exports.USERKC = process.env.USERKC;
+exports.USERPC = process.env.USERPC;
+exports.DATABASE_URL = process.env.DATABASE_URL;
